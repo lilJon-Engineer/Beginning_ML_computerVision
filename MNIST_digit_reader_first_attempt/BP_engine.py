58b7e9124535262e6b957e47ada45cfc944a5d3a
Jonathan Bigelow <jabigelow5@gmail.com>
2026-07-07 12:06:01 -0700

    Moved Raw_python_ to new folder

---
+++ MNIST_digit_reader_first_attempt/BP_engine.py
@@ -0,0 +1,7 @@
+# Take an input vector from number_import.py and compute the dot product with the weight vector. 
+# Pass this weight sum 'a' for unit 'i' through a sigmoid squashing function to produce the state of unit i. 
+# Squashing funtion is a scaled odd hyperbolic tangent function.
+# f(a) = A tanh(Sa) where A = 1.7159 and S = 2/3. (Find out why)
+# 
+
+# Cost function C = 1/P sum_p
